50474e6b3b8adfd69ba4eea0f9935d789a15d55f
Meindert Kempe <mail@mkempe.com>
2024-02-03 03:20:01 +0100

    3.3: Add simple program

--- les3/les3_3/main/les3_3.c
+++ les3/les3_3/main/les3_3.c
@@ -1,5 +1,18 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "hal/ledc_types.h"
 #include <stdio.h>
 
 void app_main(void) {
+	gpio_set_direction(GPIO_NUM_2, GPIO_MODE_INPUT_OUTPUT);
 
+	_Bool state = true;
+	for (;;) {
+		esp_err_t e = gpio_set_level(GPIO_NUM_2, state);
+
+		printf("LED toggle\n");
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		state = !state;
+	}
 }
